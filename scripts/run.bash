dccc652c4d3b3b9084101883d49ebc7092aac15c
allanjie <allanmcgrady@gmail.com>
2019-11-24 21:38:27 +0800

    remove choices and update running script

--- scripts/run.bash
+++ scripts/run.bash
@@ -4,19 +4,36 @@
 ## To run this script in background: nohup bash pipeline.bash > YOUR_LOG_FILE 2>&1 &
 
 datasets=(conll2003)
+new_types=(ORG)
+train_nums=(10 20 50 100)
+choose_new_type=1
+
 
 device=cuda:1
 negs=(0 1)
 boundarys=(0 1)
 starts=(0 1 2 3 4)
 
-run_latent_model=1
-context_emb=elmo
+run_latent_model=0
+context_emb=none
 
 if [ $run_latent_model = 0 ]
 then
     ## run baseline model
-    python3 trainer.py --dataset ${dataset} --model_folder benchmark --use_fined_labels 0 > logs/ontonotes.log --device cuda:0 2>&1
+    for (( d=0; d<${#datasets[@]}; d++ )) do
+        for (( n=0; n<${#new_types[@]}; n++ )) do
+             for (( t=0; t<${#train_nums[@]}; t++ )) do
+ 	       dataset=${datasets[$d]}
+               new_type=${new_types[$n]}
+               number=${train_nums[$t]}
+               model_folder=${dataset}_${new_type}_random_${number}_${choose_new_type}
+               data_file=${dataset}/${new_type}/few_random_${number}
+               log_file=logs/${model_folder}_choose_new_type_${choose_new_type}.log
+               python3 trainer.py --dataset ${data_file} --model_folder ${model_folder} --use_fined_labels 0 --device cuda:2 \
+                    --choose_by_new_type ${choose_new_type} > ${log_file} 2>&1 & 
+             done
+        done
+    done
 
 else
     ## Below for latent model
